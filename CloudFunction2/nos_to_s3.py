aaf1f81cd5c6eb9be0848df3072b4433d9eb5cd5
zzu-yaaa <jy122032@gmail.com>
2024-06-06 05:18:50 +0900

    Feat: Object에 대한 권한 설정

--- CloudFunction2/nos_to_s3.py
+++ CloudFunction2/nos_to_s3.py
@@ -13,7 +13,6 @@ region_name = 'kr-standard'
 access_key = os.getenv('access_key')
 secret_key = os.getenv('secret_key')
 
-
 def get_latest_object(s3, bucket_name, max_keys):
     response = s3.list_objects(Bucket=bucket_name, MaxKeys=max_keys)
 
@@ -34,6 +33,18 @@ def get_latest_object(s3, bucket_name, max_keys):
 
     return latest_object
 
+def set_object_acl_public(s3, bucket_name, object_name):
+    """
+    Set the ACL of the specified object to 'public-read'.
+
+    :param s3: Boto3 S3 client
+    :param bucket_name: Name of the S3 bucket
+    :param object_name: Name of the S3 object
+    """
+    s3.put_object_acl(Bucket=bucket_name, Key=object_name, ACL='public-read')
+    response = s3.get_object_acl(Bucket=bucket_name, Key=object_name)
+    print(f"Object ACL for {object_name} in {bucket_name}: {response}")
+
 if __name__ == "__main__":
     s3 = boto3.client(service_name, endpoint_url=endpoint_url, aws_access_key_id=access_key,
                       aws_secret_access_key=secret_key)
@@ -44,6 +55,8 @@ if __name__ == "__main__":
     latest_object = get_latest_object(s3, source_bucket_name, max_keys)
     print('Latest object: Name=%s, Size=%d, Owner=%s' % \
           (latest_object.get('Key'), latest_object.get('Size'), latest_object.get('Owner').get('ID')))
+    
+    set_object_acl_public(s3, source_bucket_name, latest_object.get('Key'))
 
     try:
         # 네이버 클라우드에서 파일 스트리밍으로 다운로드
