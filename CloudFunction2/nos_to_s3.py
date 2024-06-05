1796c866a5f3cef82a47978e696ecaa34427e4bd
zzu-yaaa <jy122032@gmail.com>
2024-06-06 06:02:06 +0900

    Feat: AWS S3에 업로드

--- CloudFunction2/nos_to_s3.py
+++ CloudFunction2/nos_to_s3.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 import os
 from botocore.exceptions import NoCredentialsError
 
-
 load_dotenv()
 
 service_name = 's3'
 endpoint_url = 'https://kr.object.ncloudstorage.com'
 region_name = 'kr-standard'
-access_key = os.getenv('access_key')
-secret_key = os.getenv('secret_key')
+naver_access_key = os.getenv('naver_access_key')
+naver_secret_key = os.getenv('naver_secret_key')
+
+aws_access_key = os.getenv('aws_access_key')
+aws_secret_key = os.getenv('aws_secret_key')
 
 def get_latest_object(s3, bucket_name, max_keys):
     response = s3.list_objects(Bucket=bucket_name, MaxKeys=max_keys)
@@ -46,30 +48,33 @@ def set_object_acl_public(s3, bucket_name, object_name):
     print(f"Object ACL for {object_name} in {bucket_name}: {response}")
 
 if __name__ == "__main__":
-    s3 = boto3.client(service_name, endpoint_url=endpoint_url, aws_access_key_id=access_key,
-                      aws_secret_access_key=secret_key)
+    naver_s3 = boto3.client(service_name, endpoint_url=endpoint_url, aws_access_key_id=naver_access_key,
+                      aws_secret_access_key=naver_secret_key)
+    
+    aws_s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
 
     source_bucket_name = 'semicolon-recording-text'
     max_keys = 300
+    target_bucket_name = 'semicolon-stt-result'
 
-    latest_object = get_latest_object(s3, source_bucket_name, max_keys)
+    latest_object = get_latest_object(naver_s3, source_bucket_name, max_keys)
     print('Latest object: Name=%s, Size=%d, Owner=%s' % \
           (latest_object.get('Key'), latest_object.get('Size'), latest_object.get('Owner').get('ID')))
     
-    set_object_acl_public(s3, source_bucket_name, latest_object.get('Key'))
+    set_object_acl_public(naver_s3, source_bucket_name, latest_object.get('Key'))
 
     try:
         # 네이버 클라우드에서 파일 스트리밍으로 다운로드
         naver_url = f"{endpoint_url}/{source_bucket_name}/{latest_object.get('Key')}"
         response = requests.get(naver_url, stream=True, headers={
-            'Authorization': access_key,
+            'Authorization': naver_access_key,
             'x-amz-date': "20160825T183244Z",
             'Host': "kr.object.ncloudstorage.com"
         })
 
         if response.status_code == 200:
             # 아마존 S3에 파일 업로드
-            #aws_s3.upload_fileobj(response.raw, aws_bucket_name, aws_object_key)
+            aws_s3.upload_fileobj(response.raw, target_bucket_name, latest_object.get('Key'))
             print("true")
             print(response.content)
             #print(f'Successfully transferred {naver_object_key} from Naver Cloud to {aws_object_key} in AWS S3.')
